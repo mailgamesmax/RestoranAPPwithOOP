d9feb072923ea5294bcdceb930acd7bc6ae19ff2
MaxPy22 <onlyinfo@ya.ru>
2023-08-11 21:53:47 +0300

    started creat Dish and Drink, wich based on Assortiment (abstract).

---
+++ RestoranAppOOP202308/RestoranAppOOP202308/models/Drink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranAppOOP202308.models
+{
+    internal class Drink : Аssortiment
+    {
+
+        public Drink() { }
+
+        public Drink(KindVariables kindVariables, string name, Dictionary<string, double> price) : base(name, price)
+        {
+            kindVariables = Аssortiment.KindVariables.Gėrimas;
+        }
+
+
+
+    }
+}
---
+++ RestoranAppOOP202308/RestoranAppOOP202308/models/Аssortiment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RestoranAppOOP202308.models.Аssortiment;
+
+namespace RestoranAppOOP202308.models
+{
+    internal abstract class Аssortiment
+    {
+        public enum KindVariables 
+        {
+            Patiekalas,
+            Gėrimas
+        }
+        public Аssortiment() { }
+
+        public Аssortiment(string name, Dictionary<string, double> nameAndPrice)
+        {
+            
+            Name = name;
+            NameAndPrice = nameAndPrice;
+            double inputPrice = 0;
+            nameAndPrice.Add(name, inputPrice);
+        }
+        public Аssortiment(string name, Dictionary<string, double> price, string description) : this( name, price)
+        {
+            Description = description;
+        }
+
+        public Аssortiment CreateAssortiment(int 1) { }
+
+            Jei nori įvesti gėrimą: 
+ControWriteLine(Įvesk pavadinimą)
+ControWriteLine(Įvesk kainą)
+switch
+ {
+	case: 1
+case: 2
+	
+
+        public string Name { get; set; }
+        public Dictionary<string, double> NameAndPrice { get; set; } //name, price
+        public string Description { get; set; }
+        public KindVariables Kind { get; set; }
+        public static List<Аssortiment> FullAssortiment { get; set; } = new List<Аssortiment>();
+    }
+}
+
